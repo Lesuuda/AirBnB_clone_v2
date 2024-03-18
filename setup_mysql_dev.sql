9bdf8619294665b03b68a7ff632a8e29564ee9ce
lesuuda <lesuudaljamani@gmail.com>
2024-03-18 14:30:00 +0300

    set up mysql environment

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- creates a database if it does not exist
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+-- creates a user if it does not exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grants all permissions on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+-- grants select permissions on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema to hbnb_dev WITH GRANT OPTION;
