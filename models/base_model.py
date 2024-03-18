fb605c7a67844c76f38996441626de44bfbe35c7
lesuuda <lesuudaljamani@gmail.com>
2024-03-18 20:32:27 +0300

    updated file storage to delete objects and list all objects given class name

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,6 @@
 import uuid
 from datetime import datetime
 import models
-from models import storage
 
 
 class BaseModel:
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import json
+"""This module defines a class to manage file storage for hbnb clone"""
 
+classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+          }
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
 
+        objects_of_cls = {}
+        for key, value in self.__objects.items():
+            obj_cls_name = key.split('.')[0]
+            if obj_cls_name == cls.__name__:
+                objects_of_cls[key] = value
+        return objects_of_cls
+    
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
@@ -27,19 +47,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -48,3 +55,13 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        """
+        deletes and object from __objects dictironary
+        """
+        if obj is None:
+            return
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        if key in self.__objects:
+            del self.__objects[key]
