b1e537fbf4617d1c301ccaeff30bcbefbb8e5907
ElsanzioKE <lesanjorelly@gmail.com>
2024-03-20 00:03:32 +0300

    corrected dev.sql script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,10 @@
--- creates a database if it does not exist
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
--- creates a user if it does not exist
+-- Create or use the database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or update the user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- grants all permissions on hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
--- grants select permissions on performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema to hbnb_dev WITH GRANT OPTION;
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
