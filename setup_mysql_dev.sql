eab158ac302b58ca8b264821235ea6c0d378ae36
lesuuda <lesuudaljamani@gmail.com>
2024-03-20 10:35:27 +0300

    improved classes further

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,5 @@
--- creates a database if it does not exist
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
--- creates a user if it does not exist
+-- Prepares a MySQL server for the project.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- grants all permissions on hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
--- grants select permissions on performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema to hbnb_dev WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
