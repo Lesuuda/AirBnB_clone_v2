c6cfadcf6cabbe2832f98a5eb90053195a8b8041
lesuuda <lesuudaljamani@gmail.com>
2024-03-20 10:35:27 +0300

    improved classes further

--- models/base_model.py
+++ models/base_model.py
@@ -54,16 +54,17 @@ class BaseModel:
 
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            del dictionary['_sa_instance_state']
-        return dictionary
+        """creates dictionary of the class  and returns
+        Return:
+            returns a dictionary of all the key values in __dict__
+        """
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict.keys():
+            del my_dict['_sa_instance_state']
+        return my_dict
     
     def delete(self):
         """
--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,5 @@
--- Create or use the database
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create or update the user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant privileges
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
