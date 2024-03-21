b5b3eceefb0d80990866577cd422171b2f21998c
lesuuda <lesuudaljamani@gmail.com>
2024-03-21 13:08:56 +0300

    fixed do create to allow for creation of object parameters with parameters

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -13,4 +12,3 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
--- models/user.py
+++ models/user.py
@@ -23,5 +23,3 @@ class User(BaseModel, Base):
     last_name = Column(String(128))
     places = relationship("Place", cascade='all, delete, delete-orphan',
                             backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                            backref="user")
