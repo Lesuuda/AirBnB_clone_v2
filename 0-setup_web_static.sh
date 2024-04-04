b885949077fdc3edb01f5032da935428b2206f58
lesuuda <lesuudaljamani@gmail.com>
2024-04-04 17:21:03 +0300

    core: added configuration script for ngnix servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+apt-get update
+apt install nginx -y
+mkdir -p data/web_static/releases/test
+echo "Your Nginx server" >> data/web_static/releases/test/index.html
+ln -sf data/web_static/releases/test data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
